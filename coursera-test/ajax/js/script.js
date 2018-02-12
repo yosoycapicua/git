e2fad9ecef1bbad28ff4cefc2df5a881b32bd792
Luis Isasi <luisisasiguerrero@gmail.com>
2018-02-12 18:52:10 +0100

    updating ajax folder to lecture 57

--- coursera-test/ajax/js/script.js
+++ coursera-test/ajax/js/script.js
@@ -1,37 +1,27 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.txt", 
+            function (request) {
+              var name = request.responseText;
 
-document.addEventListener(
-	"DOMContentLoaded",
-	function (event) {
-		function sayHello(event) {
-			console.log(this);
-			console.log(event);
-			// console.log(document.getElementsByTagName("button")[0]);
+              document.querySelector("#content")
+                .innerHTML = "<h2>Hello " + name + "!</h2>";
+            });
 
-			// this.textContent = "Said it!";
-			document.getElementsByTagName("button")[0].textContent = "Already said it";
-			var name = document.getElementById("name").value;
-			var message = "<h2>Hello " + name + "!</h2>";
-
-			document.getElementById("content").innerHTML = message;
-
-			if (name === "student") {
-				document.querySelector("h1").textContent += " and still 10 to go!";
-			} else {}
-		}
-
-		document.querySelector("button").addEventListener("click", sayHello);
-		// document.querySelector("button").onclick = sayHello;
-		document.querySelector("body").addEventListener(
-			"mousemove",
-			function logMouseMovements (event) {
-				if (event.shiftKey === true) {
-					console.log("x: " + event.clientX);
-					console.log("y: " + event.clientY);
-				}
-
-			}
-		);
-
-
-	}
+        
+      });
+  }
 );
+
+
+
+
+
