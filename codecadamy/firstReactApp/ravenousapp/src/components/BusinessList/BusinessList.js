1204ce08ceb47c8db9e091fca402545b65216129
Luis Isasi <luisisasiguerrero@gmail.com>
2019-01-13 17:41:39 +0100

    23 add JSX to render

--- codecadamy/firstReactApp/ravenousapp/src/components/BusinessList/BusinessList.js
+++ codecadamy/firstReactApp/ravenousapp/src/components/BusinessList/BusinessList.js
@@ -3,7 +3,19 @@ import "./BusinessList.css"
 import Business from "../Business/Business"
 
 
-class BusinessList extends React.Component{
-    render()
+class BusinessList extends React.Component {
+    render() {
+
+        return (
+            <div className="BusinessList" >
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+            </div >)
+    }
+
 }
 
