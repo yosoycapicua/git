b40234a20dcffbf099d956c1b83aac6e79c5d34e
Luis Isasi <luisisasiguerrero@gmail.com>
2019-01-13 17:38:37 +0100

    bugfix render parenthesis Business js

--- codecadamy/firstReactApp/ravenousapp/src/components/Business/Business.js
+++ codecadamy/firstReactApp/ravenousapp/src/components/Business/Business.js
@@ -14,28 +14,30 @@ let business = {
 }
 
 class Business extends React.Component {
-    render(
-        return(
-            <div className = "Business" >
-        <div className="image-container">
-            <img src={business.imageSrc} />
-        </div>
-        <h2>{business.name}</h2>
-        <div className="Business-information">
-            <div className="Business-address">
-                <p>{business.address}</p>
-                <p>{business.city}</p>
-                <p>{business.state} {business.zipCode}</p>
+    render() {
+        return (
+            <div className="Business" >
+                <div className="image-container">
+                    <img src={business.imageSrc} />
+                </div>
+                <h2>{business.name}</h2>
+                <div className="Business-information">
+                    <div className="Business-address">
+                        <p>{business.address}</p>
+                        <p>{business.city}</p>
+                        <p>{business.state} {business.zipCode}</p>
+                    </div>
+                    <div className="Business-reviews">
+                        <h3>{business.category}</h3>
+                        <h3 className="rating">{business.rating} stars</h3>
+                        <p>{business.reviewCount} reviews</p>
+                    </div>
+                </div>
             </div>
-            <div className="Business-reviews">
-                <h3>{business.category}</h3>
-                <h3 className="rating">{business.rating} stars</h3>
-                <p>{business.reviewCount} reviews</p>
-            </div>
-        </div>
-</div>
         )
-    )
+    }
+
+
 }
 
 export default Business;
