2372f4206f6f8db13d7ad4bed2572808b4a3b58e
Luis Isasi <luisisasiguerrero@gmail.com>
2019-01-13 17:40:39 +0100

    add alt in img, nothing serious

--- codecadamy/firstReactApp/ravenousapp/src/components/Business/Business.js
+++ codecadamy/firstReactApp/ravenousapp/src/components/Business/Business.js
@@ -18,7 +18,7 @@ class Business extends React.Component {
         return (
             <div className="Business" >
                 <div className="image-container">
-                    <img src={business.imageSrc} />
+                    <img src={business.imageSrc} alt="place" />
                 </div>
                 <h2>{business.name}</h2>
                 <div className="Business-information">
