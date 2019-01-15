5b65d1b3037e286a04bc8e036f0f45d5bb2009e6
Luis Isasi <luisisasiguerrero@gmail.com>
2019-01-15 13:40:17 +0100

    42 add html to render method

--- codecadamy/firstReactApp/ravenousapp/src/components/SearchBar/SearchBar.js
+++ codecadamy/firstReactApp/ravenousapp/src/components/SearchBar/SearchBar.js
@@ -7,14 +7,26 @@ const sortByOptions = {
     "Most Reviewed": "review_count"
 }
 
-class SearchBar extends React.Component{
-    renderSortByOptions(){
-        return Object.keys(sortByOptions).map(sortByOption =>{
+class SearchBar extends React.Component {
+    renderSortByOptions() {
+        return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
             return (<li key={sortByOptionValue}>{sortByOption}</li>)
         })
     }
-    render(){
-        return
+    render() {
+        return (<div className="SearchBar">
+            <div className="SearchBar-sort-options">
+                <ul>{this.renderSortByOptions}
+          </ul>
+            </div>
+            <div className="SearchBar-fields">
+                <input placeholder="Search Businesses" />
+                <input placeholder="Where?" />
+            </div>
+            <div className="SearchBar-submit">
+                <a>Let's Go</a>
+            </div>
+        </div>)
     }
 }
