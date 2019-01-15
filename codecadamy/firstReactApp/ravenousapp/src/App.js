9508d9a8174b7d29078601177b59f8d9ceb86dd2
Luis Isasi <luisisasiguerrero@gmail.com>
2019-01-15 18:50:39 +0100

    45 import BusinessList and SearchBar

--- codecadamy/firstReactApp/ravenousapp/src/App.js
+++ codecadamy/firstReactApp/ravenousapp/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BusinessList from "./components/BusinessList/BusinessList"
+import SearchBar from "./components/SearchBar/SearchBar"
 
 class App extends Component {
   render() {
