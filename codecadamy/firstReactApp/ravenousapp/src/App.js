5f192ded2ddeccc27c75403d1d806761fc41dcea
Luis Isasi <luisisasiguerrero@gmail.com>
2019-01-21 21:03:13 +0100

    5 add business prop to BusinessList

--- codecadamy/firstReactApp/ravenousapp/src/App.js
+++ codecadamy/firstReactApp/ravenousapp/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
     return (<div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={this.businesses} />
     </div>);
   }
 }
