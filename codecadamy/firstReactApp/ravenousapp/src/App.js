e547bb98b095faee88a19581974761221f21a08d
Luis Isasi <luisisasiguerrero@gmail.com>
2019-01-21 21:44:19 +0100

    fixed typescript error

--- codecadamy/firstReactApp/ravenousapp/src/App.js
+++ codecadamy/firstReactApp/ravenousapp/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
     return (<div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList businesses={this.businesses} />
+      <BusinessList businesses={businesses} />
     </div>);
   }
 }
