7f66acd4ee8b53dd3635ccd10bc447bd094e2559
Luis Isasi <luisisasiguerrero@gmail.com>
2019-01-21 21:09:42 +0100

    7 mapbusiness

--- codecadamy/firstReactApp/ravenousapp/src/components/BusinessList/BusinessList.js
+++ codecadamy/firstReactApp/ravenousapp/src/components/BusinessList/BusinessList.js
@@ -8,7 +8,7 @@ class BusinessList extends React.Component {
 
         return (
             <div className="BusinessList" >
-                {this.props.businesses.map()}
+                {this.props.businesses.map(business)}
             </div >)
     }
 
