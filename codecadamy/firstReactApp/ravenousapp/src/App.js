386c33ff4e0c500b7024f4be9faf983efcd79217
yosoycapicua <luisisasiguerrero@gmail.com>
2019-01-26 18:17:52 +0100

    19 add searchYelp prop to SearchBar component

--- codecadamy/firstReactApp/ravenousapp/src/App.js
+++ codecadamy/firstReactApp/ravenousapp/src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
   render() {
     return (<div className="App">
       <h1>ravenous</h1>
-      <SearchBar />
+      <SearchBar searchYelp={this.searchYelp} />
       <BusinessList businesses={businesses} />
     </div>);
   }
