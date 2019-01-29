7238de700531e88a39ac34e4b58341cd24907380
yosoycapicua <luisisasiguerrero@gmail.com>
2019-01-29 13:30:11 +0100

    20 export Yelp

--- Codecadamy/firstReactApp/ravenousapp/src/util/Yelp.js
+++ Codecadamy/firstReactApp/ravenousapp/src/util/Yelp.js
@@ -1,6 +1,6 @@
 const apiKey = "BPwUE121d-04QXdbt7LObIucO-nRKGFB4RHOMp3IZVeztWea_YQLRmhlZ3S1bo2H66p1MjOBn5MfeAn9dkKaY5FpjYunctwwB8rAp023eVUyB9jRuLltOnwyN2NPXHYx"
 
-let Yelp = {
+export let Yelp = {
     search(term, location, sortBy) {
         return fetch(
             `https://cors-anywhere.herokuapp.com/
