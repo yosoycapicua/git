9b71503182e28d93343fbcae267c0993ad6c45c4
yosoycapicua <luisisasiguerrero@gmail.com>
2019-01-29 13:06:36 +0100

    11 added quotes

--- Codecadamy/firstReactApp/ravenousapp/src/util/Yelp.js
+++ Codecadamy/firstReactApp/ravenousapp/src/util/Yelp.js
@@ -2,8 +2,7 @@ const apiKey = "BPwUE121d-04QXdbt7LObIucO-nRKGFB4RHOMp3IZVeztWea_YQLRmhlZ3S1bo2H
 
 let Yelp = {
     search(term, location, sortBy){
-        return fetch(https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}
-            )
+        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`)
     }
 }
 
