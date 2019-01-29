a5cf49fda1129da10cc4e194e49658c37e9a3404
yosoycapicua <luisisasiguerrero@gmail.com>
2019-01-29 18:33:31 +0100

    bugfix add this.state to the rendering of BusinessList

--- codecadamy/firstReactApp/ravenousapp/src/App.js
+++ codecadamy/firstReactApp/ravenousapp/src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
     return (<div className="App">
       <h1>ravenous</h1>
       <SearchBar searchYelp={this.searchYelp} />
-      <BusinessList businesses={businesses} />
+      <BusinessList businesses={this.state.businesses} />
     </div>);
   }
 }
