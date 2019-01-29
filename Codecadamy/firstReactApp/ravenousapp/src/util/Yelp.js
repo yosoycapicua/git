27fc1d0063cc0dbff6904dd0cda5c85714ccd1f3
yosoycapicua <luisisasiguerrero@gmail.com>
2019-01-29 18:28:59 +0100

    bugfix spaces in uri

--- Codecadamy/firstReactApp/ravenousapp/src/util/Yelp.js
+++ Codecadamy/firstReactApp/ravenousapp/src/util/Yelp.js
@@ -3,8 +3,7 @@ const apiKey = "BPwUE121d-04QXdbt7LObIucO-nRKGFB4RHOMp3IZVeztWea_YQLRmhlZ3S1bo2H
 export let Yelp = {
     search(term, location, sortBy) {
         return fetch(
-            `https://cors-anywhere.herokuapp.com/
-        https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+            `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
             {
                 headers: {
                     Authorization: `Bearer ${apiKey}`
