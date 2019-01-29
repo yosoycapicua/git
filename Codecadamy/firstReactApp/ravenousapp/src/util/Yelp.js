3d168b73d795e7bbda740e2c82ac4d2a5c6c0fd8
yosoycapicua <luisisasiguerrero@gmail.com>
2019-01-29 13:11:43 +0100

    13 fetch headers apiKey

--- Codecadamy/firstReactApp/ravenousapp/src/util/Yelp.js
+++ Codecadamy/firstReactApp/ravenousapp/src/util/Yelp.js
@@ -2,8 +2,14 @@ const apiKey = "BPwUE121d-04QXdbt7LObIucO-nRKGFB4RHOMp3IZVeztWea_YQLRmhlZ3S1bo2H
 
 let Yelp = {
     search(term, location, sortBy){
-        return fetch(`https://cors-anywhere.herokuapp.com/
-        https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`)
+        return fetch(
+            `https://cors-anywhere.herokuapp.com/
+        https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        })
     }
 }
 
